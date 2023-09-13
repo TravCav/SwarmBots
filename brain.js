3f5d017fd47943075d92dc305ea3b1685fa079ae
TravCav <xurrux@gmail.com>
2023-09-13 01:40:28 -0400

    more brain

--- brain.js
+++ brain.js
@@ -1,7 +1,7 @@
 class Brain {
   constructor() {
     this.inputCount = 12;
-    const hiddenCount = 30;
+    const hiddenCount = 50;
     this.neurons = new Array(this.inputCount + hiddenCount + 8);
 
     //16 inputs
